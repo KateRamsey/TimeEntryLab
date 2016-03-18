096ae9f8b7a950420bb4bb5d1e53349449aaa6e1
Kate Ramsey <kateramsey@live.com>
2016-03-18 15:57:51 -0500

    Random data populates the Developer table

--- TimeEntryLab/Migrations/Configuration.cs
+++ TimeEntryLab/Migrations/Configuration.cs
@@ -1,3 +1,5 @@
+using FizzWare.NBuilder;
+
 namespace TimeEntryLab.Migrations
 {
     using System;
@@ -10,9 +12,10 @@ namespace TimeEntryLab.Migrations
         public Configuration()
         {
             AutomaticMigrationsEnabled = true;
+            AutomaticMigrationDataLossAllowed = true;
         }
 
-        protected override void Seed(TimeEntryLab.Model1 context)
+        protected override void Seed(TimeEntryLab.Model1 db)
         {
             //  This method will be called after migrating to the latest version.
 
@@ -35,10 +38,29 @@ namespace TimeEntryLab.Migrations
                 StartDate = new DateTime(2016, 02, 29)
             };
 
-            context.Developers.AddOrUpdate(
-                d => new {d.FirstName, d.LastName},
+            db.Developers.AddOrUpdate(
+                d => new { d.FirstName, d.LastName },
                 k
                 );
+
+            if (!db.Developers.Any())
+            {
+                var developers = Builder<Developer>.CreateListOfSize(10)
+                        .All()
+                        .With(m => m.FirstName = Faker.NameFaker.FirstName())
+                        .With(m => m.LastName = Faker.NameFaker.LastName())
+                        .With(m => m.Email = Faker.InternetFaker.Email())
+                        .With(m => m.StartDate = Faker.DateTimeFaker.DateTime(new DateTime(1985, 01, 01), DateTime.Now))
+
+
+                        .Build();
+
+                db.Developers.AddOrUpdate(c => c.Id, developers.ToArray());
+            }
+
+
+
         }
     }
 }
+
--- TimeEntryLab/Program.cs
+++ TimeEntryLab/Program.cs
@@ -4,6 +4,7 @@ using System.Data.Entity;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using FizzWare.NBuilder;
 
 namespace TimeEntryLab
 {
@@ -11,6 +12,18 @@ namespace TimeEntryLab
     {
         static void Main(string[] args)
         {
+
+            var developers = Builder<Developer>.CreateListOfSize(10)
+                    .All()
+                    .With(m => m.FirstName = Faker.NameFaker.FirstName())
+                    .With(m => m.LastName = Faker.NameFaker.LastName())
+                    .With(m => m.Email = Faker.InternetFaker.Email())
+                    .With(m => m.StartDate = Faker.DateTimeFaker.DateTime(new DateTime(1985,01,01), DateTime.Now ))
+
+
+                    .Build();
+
+
             var db = new Model1();
 
             foreach (var d in db.Developers)
